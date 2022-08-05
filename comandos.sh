1bfee98dea65a9c908d6bcd811121aa412d3885c
Luana Tonieti <luanawt43@gmail.com>
2022-08-05 16:35:42 -0300

    Adição do arquivo para instalações necessárias

--- comandos.sh
+++ comandos.sh
@@ -1,3 +1,16 @@
+blue='\e[0;34m'
+NC='\e[0m' # No Color
+#font colors:
+#Black        0;30     Dark Gray     1;30
+#Blue         0;34     Light Blue    1;34
+#Green        0;32     Light Green   1;32
+#Cyan         0;36     Light Cyan    1;36
+#Red          0;31     Light Red     1;31
+#Purple       0;35     Light Purple  1;35
+#Brown/Orange 0;33     Yellow        1;33
+#Light Gray   0;37     White         1;37
+
+echo -e "${blue} Instalação do ROS2...${NC}"
 apt-cache policy | grep universe
 sudo apt update && sudo apt install curl gnupg2 lsb-release
 sudo curl -sSL https://raw.githubusercontent.com/ros/rosdistro/master/ros.key  -o /usr/share/keyrings/ros-archive-keyring.gpg
@@ -7,6 +20,8 @@ sudo apt upgrade
 sudo apt install ros-foxy-desktop
 echo "source /opt/ros/foxy/setup.bash" >> ~/.bashrc
 sudo apt install python3-colcon-common-extensions
+
+echo -e "${blue} Instalação das bibliotecas necessárias...${NC}"
 sudo apt install python3-pip
 sudo pip install construct
 sudo pip3 install opencv-python
